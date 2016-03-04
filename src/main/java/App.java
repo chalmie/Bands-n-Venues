998210a2bae836073f86ae9b94c02f1228d108d4
chalmie <blairchalmers@gmail.com>
2016-03-04 11:38:15 -0800

    add post path to venue.vtl-update name

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -99,7 +99,7 @@ public class App {
         model.put("bands", Band.all());
         return new ModelAndView(model, layout);
       }, new VelocityTemplateEngine());
-  
+
       post("/venue/update/:id", (request, response) -> {
         HashMap<String, Object> model = new HashMap<String, Object>();
         int id = Integer.parseInt(request.params("id"));
@@ -112,19 +112,19 @@ public class App {
         model.put("bands", Band.all());
         return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
-  //
-  //       post("/venue/update/name/:id", (request, response) -> {
-  //         HashMap<String, Object> model = new HashMap<String, Object>();
-  //         int id = Integer.parseInt(request.params("id"));
-  //         Venue myVenue = Venue.find(id);
-  //         String inputName = request.queryParams("name");
-  //         myVenue.update(inputName);
-  //         model.put("venue", myVenue);
-  //         model.put("bands", Band.all());
-  //         response.redirect("/");
-  //         return null;
-  //       });
-  //
+  
+        post("/venue/update/name/:id", (request, response) -> {
+          HashMap<String, Object> model = new HashMap<String, Object>();
+          int id = Integer.parseInt(request.params("id"));
+          Venue myVenue = Venue.find(id);
+          String inputName = request.queryParams("name");
+          myVenue.update(inputName);
+          model.put("venue", myVenue);
+          model.put("bands", Band.all());
+          response.redirect("/");
+          return null;
+        });
+
   //     post("/venue/delete/:id", (request, response) -> {
   //       HashMap<String, Object> model = new HashMap<String, Object>();
   //       int id = Integer.parseInt(request.params("id"));
