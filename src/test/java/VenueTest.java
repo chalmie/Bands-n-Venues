2df41fee99f3d5fe8a7174093b7a6265f3546671
chalmie <blairchalmers@gmail.com>
2016-03-04 11:14:53 -0800

    add all method to venue

--- src/test/java/VenueTest.java
+++ src/test/java/VenueTest.java
@@ -8,10 +8,10 @@ public class VenueTest {
   @Rule
   public DatabaseRule database = new DatabaseRule();
 
-  // @Test
-  // public void all_emptyAtFirst() {
-  //   assertEquals(Venue.all().size(), 0);
-  // }
+  @Test
+  public void all_emptyAtFirst() {
+    assertEquals(Venue.all().size(), 0);
+  }
 
   // @Test
   // public void equals_returnsTrueIfDescriptionsAretheSame() {
