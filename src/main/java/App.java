4ca5489088bfc6218da34a7e314402976738a53a
chalmie <blairchalmers@gmail.com>
2016-03-04 11:12:57 -0800

    add initial files

---
+++ src/main/java/App.java
@@ -0,0 +1,139 @@
+import java.util.HashMap;
+import java.util.List;
+
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
+
+public class App {
+  public static void main(String[] args) {
+    staticFileLocation("/public/");
+    String layout = "templates/layout.vtl";
+
+  //   get("/", (request, response) -> {
+  //     HashMap<String, Object> model = new HashMap<String, Object>();
+  //     model.put("template", "templates/index.vtl");
+  //     model.put("bands", Band.all());
+  //     model.put("venues", Venue.all());
+  //     return new ModelAndView(model, layout);
+  //   }, new VelocityTemplateEngine());
+  //
+  //   post("/add/band", (request, response) -> {
+  //     HashMap<String, Object> model = new HashMap<String, Object>();
+  //     String inputName = request.queryParams("name");
+  //     Band newBand = new Band(inputName);
+  //     newBand.firstToUppercase();
+  //     newBand.save();
+  //     model.put("band", newBand);
+  //     model.put("bands", Band.all());
+  //     model.put("venues", Venue.all());
+  //     model.put("template", "templates/index.vtl");
+  //     return new ModelAndView(model, layout);
+  //   }, new VelocityTemplateEngine());
+  //
+  //   post("/add/venue", (request, response) -> {
+  //     HashMap<String, Object> model = new HashMap<String, Object>();
+  //     String inputName = request.queryParams("name");
+  //     Venue newVenue = new Venue(inputName);
+  //     newVenue.firstToUppercase();
+  //     newVenue.save();
+  //     model.put("venue", newVenue);
+  //     model.put("bands", Band.all());
+  //     model.put("venues", Venue.all());
+  //     model.put("template", "templates/index.vtl");
+  //     return new ModelAndView(model, layout);
+  //   }, new VelocityTemplateEngine());
+  //
+  //   get("/band/:id", (request, response) -> {
+  //     HashMap<String, Object> model = new HashMap<String, Object>();
+  //     int id = Integer.parseInt(request.params("id"));
+  //     Band myBand = Band.find(id);
+  //     model.put("band", myBand);
+  //     model.put("template", "templates/band.vtl");
+  //     model.put("venues", Venue.all());
+  //     return new ModelAndView(model, layout);
+  //   }, new VelocityTemplateEngine());
+  //
+  //   post("/band/update/:id", (request, response) -> {
+  //     HashMap<String, Object> model = new HashMap<String, Object>();
+  //     int id = Integer.parseInt(request.params("id"));
+  //     Band myBand = Band.find(id);
+  //     int venueId = Integer.parseInt(request.queryParams("venue_id"));
+  //     Venue myVenue = Venue.find(venueId);
+  //     myBand.addVenue(myVenue);
+  //     model.put("band", myBand);
+  //     model.put("template", "templates/band.vtl");
+  //     model.put("venues", Venue.all());
+  //     return new ModelAndView(model, layout);
+  //     }, new VelocityTemplateEngine());
+  //
+  //   post("/band/update/name/:id", (request, response) -> {
+  //     HashMap<String, Object> model = new HashMap<String, Object>();
+  //     int id = Integer.parseInt(request.params("id"));
+  //     Band myBand = Band.find(id);
+  //     String inputName = request.queryParams("name");
+  //     myBand.update(inputName);
+  //     model.put("band", myBand);
+  //     model.put("venues", Venue.all());
+  //     response.redirect("/");
+  //     return null;
+  //   });
+  //
+  //
+  //   post("/band/delete/:id", (request, response) -> {
+  //     HashMap<String, Object> model = new HashMap<String, Object>();
+  //     int id = Integer.parseInt(request.params("id"));
+  //     Band myBand = Band.find(id);
+  //     myBand.delete();
+  //     model.put("band", myBand);
+  //       response.redirect("/");
+  //       return null;
+  //     });
+  //
+  //     get("/venue/:id", (request, response) -> {
+  //       HashMap<String, Object> model = new HashMap<String, Object>();
+  //       int id = Integer.parseInt(request.params("id"));
+  //       Venue myVenue = Venue.find(id);
+  //       model.put("venue", myVenue);
+  //       model.put("template", "templates/venue.vtl");
+  //       model.put("bands", Band.all());
+  //       return new ModelAndView(model, layout);
+  //     }, new VelocityTemplateEngine());
+  //
+  //     post("/venue/update/:id", (request, response) -> {
+  //       HashMap<String, Object> model = new HashMap<String, Object>();
+  //       int id = Integer.parseInt(request.params("id"));
+  //       Venue myVenue = Venue.find(id);
+  //       int bandId = Integer.parseInt(request.queryParams("band_id"));
+  //       Band myBand = Band.find(bandId);
+  //       myVenue.addBand(myBand);
+  //       model.put("venue", myVenue);
+  //       model.put("template", "templates/venue.vtl");
+  //       model.put("bands", Band.all());
+  //       return new ModelAndView(model, layout);
+  //       }, new VelocityTemplateEngine());
+  //
+  //       post("/venue/update/name/:id", (request, response) -> {
+  //         HashMap<String, Object> model = new HashMap<String, Object>();
+  //         int id = Integer.parseInt(request.params("id"));
+  //         Venue myVenue = Venue.find(id);
+  //         String inputName = request.queryParams("name");
+  //         myVenue.update(inputName);
+  //         model.put("venue", myVenue);
+  //         model.put("bands", Band.all());
+  //         response.redirect("/");
+  //         return null;
+  //       });
+  //
+  //     post("/venue/delete/:id", (request, response) -> {
+  //       HashMap<String, Object> model = new HashMap<String, Object>();
+  //       int id = Integer.parseInt(request.params("id"));
+  //       Venue myVenue = Venue.find(id);
+  //       myVenue.delete();
+  //       model.put("venue", myVenue);
+  //         response.redirect("/");
+  //         return null;
+  //       });
+  }
+}
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,67 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.fluentlenium.adapter.FluentTest;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.apache.commons.lang.WordUtils;
+
+public class AppTest extends FluentTest {
+  public WebDriver webDriver = new HtmlUnitDriver();
+  public WebDriver getDefaultDriver() {
+      return webDriver;
+  }
+
+  @ClassRule
+  public static ServerRule server = new ServerRule();
+
+  // @Test
+  // public void rootTest() {
+  //   goTo("http://localhost:4567/");
+  //   assertThat(pageSource()).contains("");
+  // }
+  //
+  // @Test
+  // public void addsBandAndDisplaysOnSamePage_true() {
+  //   goTo("http://localhost:4567/");
+  //   Band newBand = new Band("brand new");
+  //   newBand.firstToUppercase();
+  //   newBand.save();
+  //   goTo("http://localhost:4567/");
+  //   assertThat(pageSource()).contains("Brand New");
+  // }
+  //
+  // @Test
+  // public void addsVenueAndDisplaysOnSamePage_true() {
+  //   goTo("http://localhost:4567/");
+  //   Venue newVenue = new Venue("modacenter");
+  //   newVenue.firstToUppercase();
+  //   newVenue.save();
+  //   goTo("http://localhost:4567/");
+  //   assertThat(pageSource()).contains("Modacenter");
+  // }
+  // //False Positive
+  // @Test
+  // public void bandIsDeletedTest() {
+  //   String path = "http://localhost:4567/";
+  //   goTo(path);
+  //   Band myBand = new Band("Brand New");
+  //   myBand.save();
+  //   myBand.delete();
+  //   goTo(path);
+  //   assertThat((pageSource()).contains("Brand New") == false);
+  // }
+  // // False Positive
+  // @Test
+  // public void venueIsDeletedTest() {
+  //   String path = "http://localhost:4567/";
+  //   goTo(path);
+  //   Venue myVenue = new Venue("Modacenter");
+  //   myVenue.save();
+  //   myVenue.delete();
+  //   goTo(path);
+  //   assertThat((pageSource()).contains("Modacenter") == false);
+  // }
+}
